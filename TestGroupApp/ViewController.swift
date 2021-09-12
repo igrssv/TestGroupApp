3229ed9a101ee4914e87261ffbc54bccfdc883d5
igor <igro.sia@yandex.ru>
2021-09-12 12:54:06 +0300

    Initial Commit

---
+++ TestGroupApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGroupApp
+//
+//  Created by Игорь Сысоев on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
